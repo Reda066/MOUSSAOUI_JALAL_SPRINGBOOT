fa4b58fcb099c4a4439879616bb199b1ecceffd1
JALAL Reda <reda.jalal@soprasteria.com>
2020-10-03 18:22:58 +0200

    modif .sql

--- spring-boot-microservices-master/product/src/main/resources/data.sql
+++ spring-boot-microservices-master/product/src/main/resources/data.sql
@@ -1,11 +1,11 @@
 INSERT INTO PRODUCT(id, name , description, illustration, price) values
-(0, 'Nom de produit', 'Description qui donne envie d''acheter', 'https://picsum.photos/300/300', 150),
-(1, 'Nom de produit', 'Description qui donne envie d''acheter', 'https://picsum.photos/300/300', 150),
-(2, 'Nom de produit', 'Description qui donne envie d''acheter', 'https://picsum.photos/300/300', 150),
-(3, 'Nom de produit', 'Description qui donne envie d''acheter', 'https://picsum.photos/300/300', 150),
-(4, 'Nom de produit', 'Description qui donne envie d''acheter', 'https://picsum.photos/300/300', 150),
-(5, 'Nom de produit', 'Description qui donne envie d''acheter', 'https://picsum.photos/300/300', 150),
-(6, 'Nom de produit', 'Description qui donne envie d''acheter', 'https://picsum.photos/300/300', 150),
-(7, 'Nom de produit', 'Description qui donne envie d''acheter', 'https://picsum.photos/300/300', 150),
-(8, 'Nom de produit', 'Description qui donne envie d''acheter', 'https://picsum.photos/300/300', 150);
+(0, 'Nokia Home', 'La caméra de sécurité Nokia Home est à la fois élégante et efficace. Elle innove même en intégrant un capteur de particules afin de vous informer de la qualité de l''air ambiant.','https://www.objeko.com/wp-content/uploads/2017/01/Nokia-Home-1024x1024.jpg', 99),
+(1, 'Qivio', 'Le thermostat connecté Qivivo est assez inédit dans son domaine puisque sa dernière version permet de contrôler les chauffages électriques via fil pilote.', 'https://www.comapsmarthome.com/front/images/products/qivivo-thermostat-2.jpg', 150),
+(2, 'Enceinte Bluetooth Tweddr', 'Cette enceinte sans fil à 360 degrés est capable de produire un son explosif, Enceinte bluetooth portable fournit deux puissants transducteurs de 5 W et deux radiateurs passifs externes, délivrant des graves robustes et des aigus cristallins.', 'https://images-na.ssl-images-amazon.com/images/I/71fNKuUW8uL._AC_SY355_.jpg', 37),
+(3, 'Starter Kit Philips Hue', 'Envie de changer votre ambiance d''intérieur ? Le kit de démarrage d''ampoules Philips Hue est fait pour vous. Une fois les équipements connectés à la base,il ne vous reste plus qu''à contrôler les ampoules connectées avec votre tablette.', 'https://images-na.ssl-images-amazon.com/images/I/81Q0%2BHS5SXL._AC_SX679_.jpg', 199),
+(4, 'Amazon Echo Spot', 'L''Echo Spot créé par Amazon est plus qu''une enceinte connectée. Ce petit appareil élégant embarque un écran tactile qui permet l''affichage de nombreuses informations. Son haut-parleur est suffisant pour sonoriser une chambre. ', 'https://www.objeko.com/wp-content/uploads/2018/06/Echo-Spot-1024x1024.jpg', 129),
+(5, 'Huawei Watch GT 2e', 'Si vous recherchez une montre qui soit à la fois élégante et qui vous permette de garder un œil sur votre forme physique, vous êtes au bon endroit.Le cadre en acier inoxydable est parfaitement assorti aux bracelets.', 'https://www.powerplanetonline.com/cdnassets/huawei_watch_gt_2e_icy_white_01_ad_l.jpg', 129),
+(6, 'Netgear Arlo Ultra 4K 100% sans-fil', 'Là où la plupart des caméras connectées utilisent une alimentation filaire, Netgear fait le pari un peu fou du 100% sans-fil. La solution Arlo Ultra 4K comporte une base qui peut ensuite contrôler plusieurs caméras.', 'https://www.objeko.com/wp-content/uploads/2017/11/Arlo-Ultra-4K-1024x835.jpg', 499),
+(7, 'Homido V2', 'Le casque de réalité virtuelle Homido en est maintenant à sa seconde version. Ce casque conçu par une start-up française permet d''utiliser n''importe quel smartphone de 4.5″ à 6″. De plus, les lentilles et sangles sont réglables.', 'https://www.cdiscount.com/pdt2/1/4/3/1/700x700/hom3760071190143/rw/homido-v2-casque-de-realite-virtuelle-noir.jpg', 49),
+(8, 'DJI Mavic 2 Pro', 'Le Mavic 2 Pro de DJI est la Rolls Royce des drones : caméra 4K, évitement d’obstacles, retour automatique, etc… Truffé de capteurs, ce drone possède en plus une autonomie conséquente de 31 minutes en vol.', 'https://static.fnac-static.com/multimedia/Images/FR/MDM/62/8d/7f/8359266/1540-1/tsp20200610083352/Drone-4K-DJI-Mavic-2-Pro.jpg', 1500);
 
